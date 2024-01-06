7e99eb07c2e446a1c5177ed0c4773a8413c4c919
Rahul Raniwal <raniwalrahul2019@gmail.com>
2024-01-06 22:46:06 +0530

    jsLearn folder created

---
+++ jsLearn/eps-10.js
@@ -0,0 +1,33 @@
+// function x() {
+//   var a = 7;
+//   function y() {
+//     console.log(a);
+//   }
+//   y();
+// }
+// x();
+
+// function x() {
+//   var a = 7;
+//   function y() {
+//     console.log(a);
+//   }
+//   return y;
+// }
+
+// var z = x();
+// console.log(z);
+// z();
+
+function z() {
+  var b = 100;
+  function x() {
+    var a = 7;
+    function y() {
+      console.log(a, b);
+    }
+    y();
+  }
+  x();
+}
+z();
