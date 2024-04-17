9b2c231482cc970df08bf076bf6788d334e99bed
Rahul Raniwal <raniwalrahul2019@gmail.com>
2024-04-17 12:20:39 +0530

    TS done till fucntion

--- typescriptLearn/hello world/dist/index.js
+++ typescriptLearn/hello world/dist/index.js
@@ -1,7 +1,18 @@
 "use strict";
-let a = 69;
-if (a < 90) {
-    a += 10;
-}
-console.log(a);
+let number = [1, 2, 3, 4, 5];
+let user = [1, "Rahul"];
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+var Size2;
+(function (Size2) {
+    Size2["Small"] = "s";
+    Size2["Medium"] = "m";
+    Size2["Large"] = "l";
+})(Size2 || (Size2 = {}));
+let mySize = Size.Medium;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- typescriptLearn/hello world/src/index.ts
+++ typescriptLearn/hello world/src/index.ts
@@ -1,7 +1,61 @@
-// console.log("Hello world");
+// // console.log("Hello world");
 
-let a: number = 69;
-if (a < 90) {
-  a += 10;
+// let a: number = 69;
+// if (a < 90) {
+//   a += 10;
+// }
+// console.log(a);
+
+// let sales = 123_456_789; // number type
+// let course = "TypeScript"; // string
+// let isPublished = true; // boolean
+// let level; // any
+
+// function render(docs) {
+//   console.log(docs);
+// }
+
+/****************** ARRAY ******************/
+
+// let number: number[] = [1, 2, 3, 4, 5];
+
+// /****************** Tupple **********************/
+// // tuple is like fixed length array but they are usefull when we have key value pair
+
+// let user: [number, string] = [1, "Rahul"];
+
+// /******************* ENUM **********************/
+
+// const enum Size {
+//   Small = 1,
+//   Medium,
+//   Large,
+// } // next value will be in continuation like 2 3
+// const enum Size2 {
+//   Small = "s",
+//   Medium = "m",
+//   Large = "l",
+// }
+
+// let mySize: Size = Size.Medium;
+// console.log(mySize);
+
+/****************** Function *************************/
+
+// function calculateTax(income: number, taxYear: number): number {
+//   if (taxYear < 2021) {
+//     return income * 1.2;
+//   }
+//   return income * 1.3;
+// }
+
+// calculateTax(60_00_00, 2023);
+
+function calculateTax(income: number, taxYear = 2023): number {
+  if (taxYear < 2021) {
+    return income * 1.2;
+  }
+  return income * 1.3;
 }
-console.log(a);
+
+calculateTax(60_00_00, 2023);
