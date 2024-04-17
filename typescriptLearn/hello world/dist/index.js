864ca18117272f0cc2a325d32b51ce741610d097
Rahul Raniwal <raniwalrahul2019@gmail.com>
2024-04-17 13:03:19 +0530

    ts advance topics done

--- typescriptLearn/hello world/dist/index.js
+++ typescriptLearn/hello world/dist/index.js
@@ -1,18 +1,12 @@
 "use strict";
-let number = [1, 2, 3, 4, 5];
-let user = [1, "Rahul"];
-var Size;
-(function (Size) {
-    Size[Size["Small"] = 1] = "Small";
-    Size[Size["Medium"] = 2] = "Medium";
-    Size[Size["Large"] = 3] = "Large";
-})(Size || (Size = {}));
-var Size2;
-(function (Size2) {
-    Size2["Small"] = "s";
-    Size2["Medium"] = "m";
-    Size2["Large"] = "l";
-})(Size2 || (Size2 = {}));
-let mySize = Size.Medium;
-console.log(mySize);
+function kgToLbs(weight) {
+    if (typeof weight === "number") {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
+}
+kgToLbs(10);
+kgToLbs("10kg");
 //# sourceMappingURL=index.js.map
--- typescriptLearn/hello world/src/index.ts
+++ typescriptLearn/hello world/src/index.ts
@@ -51,11 +51,92 @@
 
 // calculateTax(60_00_00, 2023);
 
-function calculateTax(income: number, taxYear = 2023): number {
-  if (taxYear < 2021) {
-    return income * 1.2;
-  }
-  return income * 1.3;
-}
+// function calculateTax(income: number, taxYear = 2023): number {
+//   if (taxYear < 2021) {
+//     return income * 1.2;
+//   }
+//   return income * 1.3;
+// }
 
-calculateTax(60_00_00, 2023);
+// calculateTax(60_00_00, 2023);
+
+/**************** Object ***********************/
+
+// let employee: {
+//   readonly id: number;
+//   name: string;
+//   retire: (date: Date) => void;
+// } = {
+//   id: 1,
+//   name: "Raniwal",
+//   retire: (date: Date) => {
+//     console.log(date);
+//   },
+// };
+
+// employee.name = "Rahul";
+// console.log(employee.name);
+
+/**************** Type Alias *******************/
+
+// type Employee = {
+//   readonly id: number;
+//   name: string;
+//   retire: (date: Date) => void;
+// };
+
+// let employee1: Employee = {
+//   id: 1,
+//   name: "Mosh",
+//   retire: (date: Date) => {
+//     console.log(date);
+//   },
+// };
+
+// /***************** Union Type ******************/
+// function kgToLbs(weight: number | string): number {
+//   // Narrowing
+//   if (typeof weight === "number") {
+//     return weight * 2.2;
+//   } else {
+//     return parseInt(weight) * 2.2;
+//   }
+// }
+
+// kgToLbs(10);
+// kgToLbs("10kg");
+
+/****************** InterSection Type *********************/
+
+// type Draggable = {
+//   drag: () => void;
+// };
+
+// type Resizable = {
+//   resize: () => void;
+// };
+
+// type UiWidget = Draggable & Resizable;
+
+// let textBox: UiWidget = {
+//   drag: () => {},
+//   resize: () => {},
+// };
+
+// /*************** Literal Type ********************/
+// type Quantity = 50 | 20;
+// let quantity: Quantity = 20;
+
+/************* Nullable Type *************/
+
+// function greet(name: string | null) {
+//   if (name) {
+//     console.log(name.toUpperCase());
+//   } else {
+//     console.log("Hola Amigo!!");
+//   }
+// }
+
+// greet(null);
+
+/************* Optional Chaining****************/
