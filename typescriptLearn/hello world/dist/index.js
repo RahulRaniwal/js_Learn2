d0fc392451ee2cd99bd601011721d908869f35f1
Rahul Raniwal <raniwalrahul2019@gmail.com>
2024-04-17 11:19:27 +0530

    basic of ts covered

---
+++ typescriptLearn/hello world/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let a = 69;
+if (a < 90) {
+    a += 10;
+}
+console.log(a);
+//# sourceMappingURL=index.js.map
---
+++ typescriptLearn/hello world/src/index.ts
@@ -0,0 +1,7 @@
+// console.log("Hello world");
+
+let a: number = 69;
+if (a < 90) {
+  a += 10;
+}
+console.log(a);
