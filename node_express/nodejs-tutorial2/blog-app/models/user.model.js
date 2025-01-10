6385f25c6aa3c896de38029d7c6630144e7e3bbe
Rahul Raniwal <raniwalrahul2019@gmail.com>
2025-01-10 15:56:29 +0530

    file restored successfully

---
+++ node_express/nodejs-tutorial2/blog-app/models/user.model.js
@@ -0,0 +1,84 @@
+const { Schema, model } = require("mongoose");
+const { createHmac, randomBytes } = require("crypto");
+const { createTokenForUser } = require("../services/authentication.service.js");
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 5,
+    },
+    profileImageUrl: {
+      type: String,
+      default: "/images/user.png",
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return next(); // if password feilds is unchanged
+
+  const salt = randomBytes(16).toString("hex");
+
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+  user.salt = salt;
+  user.password = hashedPassword;
+  next();
+});
+
+userSchema.statics.matchPasswordAndGenerateToken = async function (
+  email,
+  password
+) {
+  const user = await this.findOne({ email });
+  if (!user) throw new Error("No such user found");
+
+  const salt = user.salt;
+  const hashedPassword = user.password;
+
+  const hashedProvidedPassword = createHmac("sha256", salt)
+    .update(password)
+    .digest("hex");
+
+  if (hashedPassword !== hashedProvidedPassword) {
+    throw new Error("Incorrect password");
+  }
+
+  const token = createTokenForUser(user);
+  return token;
+};
+
+const User = model("user", userSchema);
+
+module.exports = User;
---
+++ node_express/nodejs-tutorial2/blog-app/routes/user.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const { signup, login, logout } = require("../controllers/user.controller.js");
+
+// signup route
+router.get("/signup", (req, res) => {
+  return res.render("signup");
+});
+router.post("/signup", signup);
+
+// login route
+router.get("/signin", (req, res) => {
+  return res.render("signin");
+});
+router.post("/signin", login);
+
+router.get("/logout", logout);
+
+module.exports = router;
